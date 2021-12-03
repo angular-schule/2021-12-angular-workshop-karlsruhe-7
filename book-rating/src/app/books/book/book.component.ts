0469570c2f0166ad7010538a01eaeba0c00332d5
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-12-03 10:52:22 +0100

    hands on: TDD

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -11,4 +11,8 @@ export class BookComponent {
   @Input()
   book?: Book;
 
+  get stars(): undefined[] {
+    return new Array(this.book?.rating);
+  }
+
 }
---
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -0,0 +1,56 @@
+import { Book } from './book';
+import { BookRatingService } from './book-rating.service';
+
+fdescribe('BookRatingService', () => {
+  let service: BookRatingService;
+  let book: Book;
+
+  beforeEach(() => {
+    service = new BookRatingService();
+    book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3
+    }
+  });
+
+  describe('rateUp', () => {
+
+    it('should rate up a book by one', () => {
+      const ratedBook = service.rateUp(book);
+      expect(ratedBook.rating).toBe(4);
+    });
+
+    it('should not be allowed to have a rating greater than 5', () => {
+      book.rating = 5;
+      const ratedBook = service.rateUp(book);
+      expect(ratedBook.rating).toBe(5);
+    });
+
+    it('should respect immutability and return a new book', () => {
+      const ratedBook = service.rateUp(book);
+      expect(ratedBook).not.toBe(book);
+    });
+  });
+
+  describe('rateDown', () => {
+
+    it('should respect immutability and return a new book', () => {
+      const ratedBook = service.rateDown(book);
+      expect(ratedBook).not.toBe(book);
+    });
+
+    it('should rate down a book by one', () => {
+      const ratedBook = service.rateDown(book);
+      expect(ratedBook.rating).toBe(2);
+    });
+
+    it('should not be allowed to have a rating smaller than 1', () => {
+      book.rating = 1;
+      const ratedBook = service.rateDown(book);
+      expect(ratedBook.rating).toBe(1);
+    });
+  });
+
+});
