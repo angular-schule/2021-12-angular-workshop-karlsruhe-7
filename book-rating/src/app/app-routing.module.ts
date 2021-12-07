07ab808680004db0e4fe65567ec899b7dddbce61
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-12-07 14:17:05 +0100

    routing: lazy loading

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'books' }
+  // { path: '', pathMatch: 'full', redirectTo: 'books' }
+  { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) },
+  { path: '**', redirectTo: 'books' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    enableTracing: true,
+    enableTracing: false,
     scrollPositionRestoration: 'top' // später 'enabled'
   })],
   exports: [RouterModule]
--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksModule } from './books/books.module';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 // import { BookComponent } from './books/book/book.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,8 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BooksModule,
+    AppRoutingModule, 
     HttpClientModule
   ],
   providers: [],
