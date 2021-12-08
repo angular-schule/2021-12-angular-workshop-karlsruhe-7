072eb08737c98ddde4313c56724db971721fced6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-12-08 14:20:38 +0100

    rxjs: error handling done right

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { catchError, concatMap, map, mergeMap, of, shareReplay, switchMap } from 'rxjs';
+import { catchError, map, of, retry, switchMap } from 'rxjs';
+
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -13,11 +14,13 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn!)),
-    catchError((err: HttpErrorResponse) => of({
-      title: 'ERROR',
-      description: err.message
-    }))
+    switchMap(isbn => this.bs.getSingleBook(isbn!).pipe(
+      retry(3),
+      catchError((err: HttpErrorResponse) => of({
+        title: 'ERROR',
+        description: err.message
+      }))
+    ))
   );
 
   constructor(
