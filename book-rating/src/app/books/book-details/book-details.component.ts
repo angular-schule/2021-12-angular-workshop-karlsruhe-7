fe108caf112bec00c39a6862e9052cbf12c9eab2
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-12-08 13:44:50 +0100

    rxjs: anti-pattern -- subscribe in subscribe (without coordination)

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  constructor() { }
+  book$ = this.route.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn')),
+    map(isbn => this.bs.getSingleBook(isbn!))
+  );
 
-  ngOnInit(): void {
-  }
-
+  constructor(
+    private route: ActivatedRoute,
+    private bs: BookStoreService) { }
 }
